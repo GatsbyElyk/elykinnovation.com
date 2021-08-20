dce5c9fdcf2dc90414d971ae751f7fd35e61d05c
jdaviselyk <jdavis@elykinnovation.com>
2021-08-20 11:02:42 -0400

    Open new tabs for google links

--- src/components/contact-form/index.js
+++ src/components/contact-form/index.js
@@ -131,8 +131,8 @@ const ContactForm = ({ sectionBackgroundColor, submitButtonText }) => {
                 </div>
                 <Googletext>
                   <p>This site is protected by reCAPTCHA and the Google
-                  <a href="https://policies.google.com/privacy"> Privacy Policy</a> and
-                  <a href="https://policies.google.com/terms"> Terms of Service</a> apply.</p>
+                  <a href="https://policies.google.com/privacy" target="blank"> Privacy Policy</a> and
+                  <a href="https://policies.google.com/terms" target="blank"> Terms of Service</a> apply.</p>
                 </Googletext>
               </StyledForm>
             ) : (
